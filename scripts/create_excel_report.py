268a8c0219aff460c449e03a5376d14ce8c94ba3
APattersonBCBST <April_Patterson@bcbst.com>
2026-06-17 14:47:45 -0400

    Add script to create Excel report with test data
    Initial implementation of a script to create an Excel report with test data.

---
+++ scripts/create_excel_report.py
@@ -0,0 +1,10 @@
+
+print("Creating Excel report...")
+
+# Temporary test content
+report_data = [
+    {"member_id": "123", "amount": 100},
+    {"member_id": "456", "amount": 200}
+]
+
+print("Report created with rows:", len(report_data))
